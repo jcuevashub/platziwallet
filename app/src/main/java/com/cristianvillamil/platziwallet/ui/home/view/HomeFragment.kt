d952b00d6065236c2109c9fcfe44515de58073da
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-11 16:45:27 -0400

    Factory

--- app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/HomeFragment.kt
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cristianvillamil.platziwallet.R
 import com.cristianvillamil.platziwallet.ui.home.FavoriteTransfer
 import com.cristianvillamil.platziwallet.ui.home.HomeContract
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory.Companion.TYPE_ERROR
 import com.cristianvillamil.platziwallet.ui.home.presenter.HomePresenter
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -63,5 +65,11 @@ class HomeFragment : Fragment(), HomeContract.View {
 
     override fun showFavoriteTransfers(favoriteTransfer: List<FavoriteTransfer>) {
         favoriteTransferAdapter.setData(favoriteTransfer)
+        val dialogFactory = MessageFactory()
+        context?.let {
+            val errorDialog = dialogFactory.getDialog(it, TYPE_ERROR)
+            errorDialog.show()
+        }
+
     }
 }
